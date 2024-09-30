048a2e2e8ff572d18feee44ebb6caf80b3935e6d
Rakibul Alam <rakibulalam@Shaheens-Mac-mini.local>
2024-09-30 19:54:49 +0600

    first commit

---
+++ src/Ticket.js
@@ -0,0 +1,16 @@
+const { generate } = require("shortid");
+
+class Ticket {
+    /**
+     * Ticket contructor will receive username and price
+     * @param {string} username 
+     * @param {number} price 
+     */
+    constructor(username,price){
+        this.id = generate()
+        this.username = username;
+        this.price = price;
+        this.createdAt = new Date();
+        this.updatedAt = new Date();
+    }
+}
